b275ea77b554cbceebb99efe67426f0676c70849
MaxBondarchuk <bondarchuk.m.y@gmail.com>
2018-01-01 13:50:14 +0200

    Services Controller

---
+++ Hotel Corporate System/Controllers/ServicesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Hotel_Corporate_System.Models.Database;
+
+namespace Hotel_Corporate_System.Controllers
+{
+    public class ServicesController : Controller
+    {
+        private HotelContext db = new HotelContext();
+
+        // GET: Services
+        public ActionResult Index()
+        {
+            return View(db.Services.ToList());
+        }
+
+        // GET: Services/Details/5
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Service service = db.Services.Find(id);
+            if (service == null)
+            {
+                return HttpNotFound();
+            }
+            return View(service);
+        }
+
+        // GET: Services/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Services/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Amount")] Service service)
+        {
+            if (ModelState.IsValid)
+            {
+                service.Id = Guid.NewGuid();
+                db.Services.Add(service);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(service);
+        }
+
+        // GET: Services/Edit/5
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Service service = db.Services.Find(id);
+            if (service == null)
+            {
+                return HttpNotFound();
+            }
+            return View(service);
+        }
+
+        // POST: Services/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Amount")] Service service)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(service).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(service);
+        }
+
+        // GET: Services/Delete/5
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Service service = db.Services.Find(id);
+            if (service == null)
+            {
+                return HttpNotFound();
+            }
+            return View(service);
+        }
+
+        // POST: Services/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            Service service = db.Services.Find(id);
+            db.Services.Remove(service);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
